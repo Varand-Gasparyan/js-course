aee671217431971f69d261c60db0ef2c3b65403c
Varand <varandgasparyan@gmail.com>
2022-08-12 12:04:32 +0400

    Update bitVector.js

--- bitVector.js
+++ bitVector.js
@@ -1,12 +1,12 @@
-const NUMBER_SIZE = 32;
+const NUMBER_SIZE = 64;
 
 const createVector = (length) => {
 
     const elementCount = Math.ceil(length / NUMBER_SIZE);
-    const vector = new Array(elementCount);
+    const vector = new BigUint64Array(elementCount);
 
     for (let ix = 0; ix < elementCount; ix++)
-        vector[ix] = 0;
+        vector[ix] = BigInt(0);
 
     return vector;
 };
@@ -16,7 +16,7 @@ const addElement = (vector, index) => {
     const vectorPart = Math.floor(index / NUMBER_SIZE);
     const elementIndex = index % NUMBER_SIZE;
 
-    vector[vectorPart] = vector[vectorPart] | (1 << elementIndex);
+    vector[vectorPart] = vector[vectorPart] | BigInt(1 << elementIndex);
 };
 
 const removeElement = (vector, index) => {
@@ -24,7 +24,7 @@ const removeElement = (vector, index) => {
     const vectorPart = Math.floor(index / NUMBER_SIZE);
     const elementIndex = index % NUMBER_SIZE;
 
-    vector[vectorPart] = vector[vectorPart] & ~(1 << elementIndex);
+    vector[vectorPart] = vector[vectorPart] & BigInt(~(1 << elementIndex));
 };
 
 const getElement = (vector, index) => {
@@ -32,8 +32,8 @@ const getElement = (vector, index) => {
     const vectorPart = Math.floor(index / NUMBER_SIZE);
     const elementIndex = index % NUMBER_SIZE;
 
-    const value = vector[vectorPart] & (1 << elementIndex);
-    return value !== 0;
+    const value = vector[vectorPart] & BigInt(1 << elementIndex);
+    return value !== BigInt(0);
 };
 
 let vector = createVector(64);
